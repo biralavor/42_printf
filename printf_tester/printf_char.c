25badecf93a491984f3abe9b1cb742df4e37ae6f
umeneses <umeneses@student.42sp.org.br>
2023-10-25 15:19:43 -0300

    making tester for printf

--- printf_tester/printf_char.c
+++ printf_tester/printf_char.c
@@ -6,11 +6,12 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/24 15:36:01 by umeneses          #+#    #+#             */
-/*   Updated: 2023/10/24 15:37:11 by umeneses         ###   ########.fr       */
+/*   Updated: 2023/10/25 14:42:37 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "~/../../42_printf/ft_printf.h"
+#include "../ft_printf.h"
+#include <stdio.h>
 
 int	main(int argc, char **argv)
 {
@@ -23,8 +24,6 @@ int	main(int argc, char **argv)
 		printf("original printf sending 'string'= %s\n", argv[index]);
 		printf("\033[0;34m");
 		printf("\n");
-		// printf("my printf = ");
-		// ft_printf("%s", argv[index]);
 		index++;
 	}
 	return (0);
