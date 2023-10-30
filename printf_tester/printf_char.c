f8b25429fb901818baaaf36770c7ca98ee70550d
umeneses <umeneses@student.42sp.org.br>
2023-10-30 17:18:01 -0300

    updating my printf tester

--- printf_tester/printf_char.c
+++ printf_tester/printf_char.c
@@ -3,29 +3,28 @@
 /*                                                        :::      ::::::::   */
 /*   printf_char.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bira <bira@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/24 15:36:01 by umeneses          #+#    #+#             */
-/*   Updated: 2023/10/26 17:37:37 by bira             ###   ########.fr       */
+/*   Updated: 2023/10/30 17:14:47 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../ft_printf.h"
 #include <stdio.h>
 
-int	main(int argc, char **argv)
+int	main(void)
 {
-	int	index;
-
-	index = 0;
-	while (index < argc)
-	{
-		printf("\033[0;34m");
-		printf("original printf sending 'string'= %s\n", argv[index]);
-		printf("\033[0;33m");
-		printf("\n");
-		ft_printf("my printf = %d %c\n", 42, 'd');
-		index++;
-	}
+	printf("\033[0;34m");
+	printf("original printf checker:\n \
+			char = %c\n \
+			str  = %s\n \
+			integ= %i\n", 'd', "hello", 9);
+	printf("\033[0;33m");
+	printf("\n");
+	ft_printf("my printf :)\n \
+				char = %c\n \
+				str  = %s\n \
+				integ= %i\n", 'd', "hello", 9);
 	return (0);
 }
