7953d8288cb13c4bf9eac92505c252d7a16785c9
umeneses <umeneses@student.42sp.org.br>
2023-10-31 15:09:03 -0300

    adding %i and %d flags

--- ft_printf.c
+++ ft_printf.c
@@ -6,11 +6,11 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 15:33:49 by umeneses          #+#    #+#             */
-/*   Updated: 2023/10/30 17:10:18 by umeneses         ###   ########.fr       */
+/*   Updated: 2023/10/31 14:52:12 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_printf.h"
+#include "./ft_printf.h"
 
 int	ft_printf(const char *format, ...)
 {
@@ -19,19 +19,25 @@ int	ft_printf(const char *format, ...)
 	va_start(ptr, format);
 	while (*format != '\0')
 	{
-		if('%' == *format)
+		if ('%' == *format)
 		{
 			format++;
-			if('c' == *format)
+			if ('c' == *format)
 				ft_putchar_fd(va_arg(ptr, int), 1);
-			else if('s' == *format)
+			else if ('s' == *format)
 				ft_putstr_fd(va_arg(ptr, char *), 1);
-			else if(ft_isdigit(*format))
+			else if (('i' == *format) || ('d' == *format))
 				ft_putnbr_fd(va_arg(ptr, int), 1);
+			else if ('u' == *format)
+				ft_putnbr_fd(va_arg(ptr, int), 1);
+			// else if (45 == *format)
+			// 	ft_putchar_fd(va_arg(ptr, int), 1);
+			// else if ((48 <= *format) && (57 <= *format))
+			// 	ft_putnbr_fd(va_arg(ptr, int), 1);
 			format++;
 		}
 		else
-		{	
+		{
 			ft_putchar_fd(*format, 1);
 			format++;
 		}
--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/24 11:03:14 by umeneses          #+#    #+#             */
-/*   Updated: 2023/10/30 11:41:15 by umeneses         ###   ########.fr       */
+/*   Updated: 2023/10/31 13:11:27 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,23 +16,23 @@
 # include <stdarg.h>
 # include "./libft/libft.h"
 
-typedef	struct s_lst_1
+typedef struct s_lst_1
 {
 	int					i;
 	int					c;
 	char				*s;
 	int					d;
 	unsigned int		u;
-	struct	s_lst_1		*next;
+	struct s_lst_1		*next;
 }						t_lst_1;
 
-typedef	struct s_lst_2
+typedef struct s_lst_2
 {
 	void				*p;
 	char				*s;
 	int					hex_low;
 	int					hex_upp;
-	struct	s_lst_2		*next;
+	struct s_lst_2		*next;
 }						t_lst_2;
 
 int	ft_printf(const char *format, ...);
