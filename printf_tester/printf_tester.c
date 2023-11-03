7e49fea1a1ff30ca1cabf7480965e1c8ac8d9f13
umeneses <umeneses@student.42sp.org.br>
2023-11-03 14:27:56 -0300

    updating my printf tester

--- printf_tester/printf_tester.c
+++ printf_tester/printf_tester.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   printf_char.c                                      :+:      :+:    :+:   */
+/*   printf_tester.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/24 15:36:01 by umeneses          #+#    #+#             */
-/*   Updated: 2023/10/31 15:04:19 by umeneses         ###   ########.fr       */
+/*   Updated: 2023/11/03 14:12:27 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,21 +15,31 @@
 
 int	main(void)
 {
+	int	len;
+	int	my_l;
+
 	printf("\033[0;34m");
-	printf("original printf checker:\n \
-			char = %c\n \
-			str  = %s\n \
-			integ= %li\n \
-			decim= %d\n \
-			unsig= %u\n", \
-			'd', "string", -2147483648, __INT_MAX__, 65534);
+	len = printf(" %s\n", " NULL ");
+	printf("len = %i\n", len);
 	printf("\033[0;33m");
-	ft_printf("my printf :)\n \
-			char = %c\n \
-			str  = %s\n \
-			integ= %i\n \
-			decim= %d\n \
-			unsig= %u\n", \
-			'd', "string", -2147483648, __INT_MAX__, 65534);
+	my_l = ft_printf(" %s\n", '0' + 256);
+	printf("my_l = %i\n", my_l);
 	return (0);
 }
+/*
+{
+	printf("\033[0;34m");
+	printf("original printf checker:\n \
+		char = |%c|	str  = %s	intdec= %i\n \
+		unsig= %u	perc = %%\n \
+		hexal= %x	HEXAU = %X\n",
+		'0', "string", -32767, 65535, 55555, 44444);
+	printf("\033[0;33m");
+	ft_printf("my printf :)\n \
+		char = |%c|	str  = %s	intdec= %i\n \
+		unsig= %u	perc = %%\n \
+		hexal=		HEXAU=	\n", \
+		'0', "string", -32767, 65535);
+	return (0);
+}
+*/
