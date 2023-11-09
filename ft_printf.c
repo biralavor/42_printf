fa1b8c7a0e49b80f9802b6d41138a5e9e06c6296
umeneses <umeneses@student.42sp.org.br>
2023-11-09 14:27:36 -0300

    mandatory printf now fully operational

--- ft_printf.c
+++ ft_printf.c
@@ -6,12 +6,11 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 15:33:49 by umeneses          #+#    #+#             */
-/*   Updated: 2023/11/08 17:48:11 by umeneses         ###   ########.fr       */
+/*   Updated: 2023/11/09 13:43:06 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./ft_printf.h"
-#include <stdio.h>
 
 int	ft_printf(const char *format, ...)
 {
@@ -45,32 +44,25 @@ int	ft_placeholder(char format, va_list ptr)
 		len += ft_putchar_fd(va_arg(ptr, int), 1);
 	else if ('s' == format)
 		len += ft_putstr_fd(va_arg(ptr, char *), 1);
-	else if ('l' == format)
-	{
-		format++;
-		if (('l' == format) || ('i' == format) || ('d' == format))
-			format++;
-		len += ft_putnbr_fd(va_arg(ptr, long long int), 1);
-	}
 	else if (('i' == format) || ('d' == format))
-		len += ft_putnbrbase(va_arg(ptr, int), DECIMAL);
+		len += ft_putnbrbase(va_arg(ptr, int), DEC);
 	else if ('u' == format)
 		len += ft_putnbr_fd(va_arg(ptr, unsigned int), 1);
 	else if ('%' == format)
 		len += ft_putchar_fd(37, 1);
 	else if ('x' == format)
-		len += ft_putnbrbase((long long)va_arg(ptr, unsigned int), HEXALOWER);
+		len += ft_putnbrbase((long long)va_arg(ptr, unsigned int), HEXL);
 	else if ('X' == format)
-		len += ft_putnbrbase((long long)va_arg(ptr, unsigned int), HEXAUPPER);
+		len += ft_putnbrbase((long long)va_arg(ptr, unsigned int), HEXU);
 	else if ('p' == format)
-		len += ft_putptr((long long)va_arg(ptr, unsigned int), HEXALOWER);
+		len += ft_putptr((long long)va_arg(ptr, long long), HEXL);
 	return (len);
 }
 
-int	ft_putnbrbase(long long n, char *base)
+int	ft_putnbrbase(long long int n, char *base)
 {
-	int	len;
-	int	base_len;
+	int				len;
+	unsigned int	base_len;
 
 	len = 0;
 	base_len = ft_strlen(base);
@@ -85,19 +77,39 @@ int	ft_putnbrbase(long long n, char *base)
 	return (len);
 }
 
-int	ft_putptr(long long n, char *base)
+int	ft_put_unsigned_nbr(unsigned long long int n, char *base)
 {
-	int	len;
-	int	base_len;
+	int				len;
+	unsigned int	base_len;
 
+	len = 0;
+	base_len = ft_strlen(base);
+	if (n >= base_len)
+		len += ft_putnbrbase((n / base_len), base);
+	len += ft_putchar_fd(base[n % base_len], 1);
+	return (len);
+}
+
+int	ft_putptr(long long int n, char *base)
+{
+	int					len;
+	unsigned long int	base_len;
+	char				*hexalower;
+
+	hexalower = "0123456789abcdef";
 	len = 0;
 	base_len = ft_strlen(base);
 	if (n == 0)
 		len += ft_putstr_fd("(nil)", 1);
+	else if (base == hexalower)
+	{
+		len += ft_putstr_fd("0x", 1);
+		len += ft_put_unsigned_nbr(n, HEXL);
+	}
 	else
 	{
 		len += ft_putstr_fd("0x", 1);
-		len += ft_putnbrbase(n, base);
+		len += ft_putnbrbase(n, HEXU);
 	}
 	return (len);
 }
--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/24 11:03:14 by umeneses          #+#    #+#             */
-/*   Updated: 2023/11/08 17:47:20 by umeneses         ###   ########.fr       */
+/*   Updated: 2023/11/09 13:35:02 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,14 @@
 # include <stdarg.h>
 # include "./libft/libft.h"
 
-# define DECIMAL "0123456789"
-# define HEXALOWER "0123456789abcdef"
-# define HEXAUPPER "0123456789ABCDEF"
+# define DEC "0123456789"
+# define HEXL "0123456789abcdef"
+# define HEXU "0123456789ABCDEF"
 
 int	ft_printf(const char *format, ...);
 int	ft_placeholder(char format, va_list ptr);
-int	ft_putnbrbase(long long n, char *base);
-int	ft_putptr(long long n, char *base);
+int	ft_putnbrbase(long long int n, char *base);
+int	ft_putptr(long long int n, char *base);
+int	ft_put_unsigned_nbr(unsigned long long int n, char *base);
 
 #endif
